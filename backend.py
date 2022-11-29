494b38c061cc8adc4749a7e027a6224c3e2e9e74
Sofiia <vt211_pso@student.ztu.edu.ua>
2022-11-29 14:13:23 +0200

    off canvas with adding deleting and editing elments, page about
    deleting is not correct

--- backend.py
+++ backend.py
@@ -1,137 +1,126 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 import base64
-from itertools import islice
+# from itertools import islice
 
 app = Flask(__name__)
 client = MongoClient('mongodb+srv://RAK_MANIAK:123370@mangashelf.3ortmjy.mongodb.net/test')
 app.db = client.MangaShelf
 
+class GiveToPage:
+    Catalogue = [{'title': manga['title'], 'description': manga['description'], 'image': manga['image']} for manga in app.db.Catalogue.find({})]
+    User = 'Sign In'
 
-class User:
-    login = 'Sign In'
+    @staticmethod
+    def reloadCatalogue():
+        GiveToPage.Catalogue = [{'title': manga['title'], 'description': manga['description'], 'image': manga['image']} for manga in app.db.Catalogue.find({})]
+
+    @staticmethod
+    def get_dictionary():
+        return {'Catalogue': GiveToPage.Catalogue, 'User': GiveToPage.User}
+
+    @staticmethod
+    def get_dictionaryWithoutCatalogue():
+        return {'User': GiveToPage.User}
 
 @app.route('/')
 def outputCatalogue():
-    entry = {
-    'Catalogue': [{'title': manga['title'], 'description': manga['description'], 'image': manga['image']} for manga in app.db.Catalogue.find({})],
-    'User': User.login
-    }
-    # print(entry['Catalogue']['title'])
-    # entry['Catalogue'] = list(islice(entry['Catalogue'], 0, 10))
-    # Catalogue = [(manga[0], manga[1], manga[2]) for manga in app.db.Catalogue.find()]
-    return render_template('MangaShelf.html', entry=entry)
+    return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
 
 
 @app.route('/addElement', methods=['GET', 'POST'])
-def addToMangaShelfCatalogue():
+def addToMewMangaCatalogue():
     if request.method == 'POST':
-        entry = {
-            'Catalogue': [{'title': manga['title'], 'description': manga['description'], 'image': manga['image']} for
-                          manga in app.db.Catalogue.find({})],
-            'User': User.login
-        }
         try:
             title = request.form.get("title")
-            # miniDescription = request.form.get("miniDescription")
             description = request.form.get("description")
             img_base64 = base64.b64encode(request.files.get('image').read())
 
-            # if not title or not miniDescription or not description or not description or not img_base64 or app.db.Catalogue.find_one(
             if not title or not description or not description or not img_base64 or app.db.Catalogue.find_one({"title": title}):
-                return render_template('MangaShelf.html')
+                return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
             app.db.Catalogue.insert_one(
-                # {"title": title, "miniDescription": miniDescription, "description": description, "image": img_base64})
                 {"title": title, "description": description, "image": 'data:;base64,' + str(img_base64)[2:-1]})
-            entry = {
-                'Catalogue': [{'title': manga['title'], 'description': manga['description'], 'image': manga['image']}
-                              for manga in app.db.Catalogue.find({})],
-                'User': User.login
-            }
-            return render_template('MangaShelf.html', entry=entry)
+            GiveToPage.reloadCatalogue()
+            return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
         except:
-            return render_template('MangaShelf.html', entry=entry)
+            return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
 
 
 @app.route('/deleteElement', methods=['GET', 'POST'])
-def deleteFromMangaShelfCatalogue():
+def deleteFromMewMangaCatalogueByTitle():
     if request.method == 'POST':
-        entry = {
-            'Catalogue': [{'title': manga['title'], 'description': manga['description'], 'image': manga['image']} for
-                          manga in app.db.Catalogue.find({})],
-            'User': User.login
-        }
         try:
-            title = request.form.get("title")
+            title = request.form.get("titleDel")
             if not title or not app.db.Catalogue.find_one({"title": title}):
-                return render_template('MangaShelf.html')
+                return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
             app.db.Catalogue.delete_one({"title": title})
-            entry = {
-                'Catalogue': [{'title': manga['title'], 'description': manga['description'], 'image': manga['image']}
-                              for manga in app.db.Catalogue.find({})],
-                'User': User.login
-            }
-            return render_template('MangaShelf.html', entry=entry)
+            GiveToPage.reloadCatalogue()
+            return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
         except:
-            return render_template('MangaShelf.html', entry=entry)
+            return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
 
+@app.route('/updateElement', methods=['GET', 'POST'])
+def updateFromMewMangaCatalogueByTitle():
+    if request.method == 'POST':
+        try:
+            findByTitle = request.form.get("findByTitle")
+            if not findByTitle or not app.db.Catalogue.find_one({"title": findByTitle}):
+                return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
+
+            newTitle = request.form.get("newTitle")
+            newDescription = request.form.get("newDescription")
+            newImage = base64.b64encode(request.files.get('newImage').read())
+            if newImage:
+                # newImage = base64.b64encode(request.files.get('newImage').read())
+                app.db.Catalogue.update_one({"title": findByTitle}, {"$set": {"image": 'data:;base64,' + str(newImage)[2:-1]}})
+            if newDescription:
+                app.db.Catalogue.update_one({"title": findByTitle}, {"$set": {"description": newDescription}})
+
+            if newTitle:
+                app.db.Catalogue.update_one({"title": findByTitle}, {"$set": {"title": newTitle}})
+
+            GiveToPage.reloadCatalogue()
+            return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
+        except:
+            return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
 
 @app.route('/registration', methods=['GET', 'POST'])
 def addToMangaShelfUsers():
     if request.method == 'POST':
-        entry = {
-            'Catalogue': [{'title': manga['title'], 'description': manga['description'], 'image': manga['image']} for
-                          manga in app.db.Catalogue.find({})],
-            'User': User.login
-        }
         try:
             name = request.form.get("name")
             password = request.form.get("password")
             if not name or not password or app.db.Users.find_one({"name": name}):
                 return render_template('MangaShelf.html')
             app.db.Users.insert_one({"name": name, "password": password})
-            return render_template('MangaShelf.html', entry=entry)
+            GiveToPage.User = name
+            return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
         except:
-            return render_template('MangaShelf.html', entry=entry)
+            return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
 
 
 @app.route('/authorize', methods=['GET', 'POST'])
 def authorize():
     if request.method == 'POST':
-        entry = {
-            'Catalogue': [{'title': manga['title'], 'description': manga['description'], 'image': manga['image']} for
-                          manga in app.db.Catalogue.find({})],
-            'User': User.login
-        }
         try:
             name = request.form.get("login")
             password = request.form.get("password")
             if not name or not password or not app.db.Users.find_one({"name": name, "password": password}):
                 return render_template('MangaShelf.html')
-            User.login = name
-            entry = {
-                'User': User.login
-            }
-            return render_template('MangaShelf.html', entry=entry)
+            GiveToPage.User = name
+            return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
         except:
-            return render_template('MangaShelf.html', entry=entry)
+            return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
 
 
 @app.route('/about')
 def about():
-    entry = {
-        'User': User.login
-    }
-    return render_template('About.html', entry=entry)
+    return render_template('About.html', entry=GiveToPage.get_dictionaryWithoutCatalogue())
 
 
 @app.route('/main')
 def main():
-    entry = {
-    'Catalogue': [{'title': manga['title'], 'description': manga['description'], 'image': manga['image']} for manga in app.db.Catalogue.find({})],
-    'User': User.login
-    }
-    return render_template('MangaShelf.html', entry=entry)
+    return render_template('MangaShelf.html', entry=GiveToPage.get_dictionary())
 
 
 if __name__ == '__main__':
